5a60ec2dbbbed2bd4d3eb3074ceab8cd551e830f
balaji <76476394+Balaji041@users.noreply.github.com>
2023-09-14 09:23:18 +0530

    Create split_the_sentence-2.py

---
+++ split_the_sentence-2.py
@@ -0,0 +1,13 @@
+string=input()
+list_string=string.split()
+for i in list_string:
+    print(i)
+"""
+input: Banana Apple Pomegranate Strawberry Grapes Orange
+output:
+Banana
+Apple
+Pomegranate
+Strawberry
+Grapes
+"""
